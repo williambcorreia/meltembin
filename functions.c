07f09ee832eeb326c64c5ec213e53dc396a9674a
William Correia <wburgard6@gmail.com>
2026-08-31 21:16:41 -0300

    feat: add function to generate cue file & func to get bin sizes

--- functions.c
+++ functions.c
@@ -1,6 +1,7 @@
 #include "functions.h"
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 int checkMalloc(void *ptr)
 {
@@ -16,7 +17,7 @@ int checkFile(FILE *file)
 {
 	if (file == NULL)
 	{
-		perror("fopen");
+		perror("file");
 		return 1;
 	}
 	return 0;
@@ -40,18 +41,12 @@ char **parseCue(FILE *cueFile, size_t *binCount)
 			size_t length = end - start - 1;
 
 			char *binName = malloc(length + 1);
-			if (checkMalloc(binName)) return NULL;
 			memcpy(binName, start + 1, length);
 			binName[length] = '\0';
 
 			(*binCount)++;
 
 			char **temp = realloc(binList, (*binCount) * (sizeof(char *)));
-			if (checkMalloc(temp))
-			{
-				free(binName);
-				return NULL;
-			}
 			binList = temp;
 			binList[(*binCount) - 1] = binName;
 		}
@@ -59,7 +54,22 @@ char **parseCue(FILE *cueFile, size_t *binCount)
 	return binList;
 }
 
-char *setOutputName(char *path)
+size_t *getBinSizes(char *srcDir, char **binList, size_t binCount)
+{
+	size_t *binSizes = malloc(binCount * sizeof(size_t));
+	struct stat st;
+
+	for (size_t i = 0; i < binCount; i++)
+	{
+		char *fullPath = setFullPath(srcDir, binList[i]);
+		stat(fullPath, &st);
+		binSizes[i] = st.st_size;
+		free(fullPath);
+	}
+	return binSizes;
+}
+
+char *setOutputName(char *path, char *extension)
 {
 	char *outputName;
 	char *fileName = strrchr(path, '/');
@@ -69,8 +79,7 @@ char *setOutputName(char *path)
 
 	size_t outputNameSize = (strlen(fileName) - 4) + 4 + 1;
 	outputName = malloc(outputNameSize);
-	if (checkMalloc(outputName)) return NULL;
-	snprintf(outputName, outputNameSize, "%.*s.bin", (int)(strlen(fileName) - 4),fileName);
+	snprintf(outputName, outputNameSize, "%.*s%s", (int)(strlen(fileName) - 4), fileName, extension);
 
 	return outputName;
 }
@@ -79,7 +88,6 @@ char *setFullPath(char *dir, char *outputName)
 {
 	size_t fullPathSize = strlen(dir) + strlen(outputName) + 2;
 	char *fullPath = malloc(fullPathSize);
-	if (checkMalloc(fullPath)) return NULL;
 
 	if (dir[strlen(dir) - 1] == '/') 
 	{
@@ -104,6 +112,59 @@ void copyFile(FILE *source, FILE *destination)
 		while(bytesWritten < bytesRead)
 		{
 			bytesWritten += fwrite(fileBuffer + bytesWritten, 1, bytesRead - bytesWritten, destination);
+		}
+	}
+}
+
+void generateCue(FILE *srcCue, FILE *dstCue, size_t *binSizes)
+{
+	char line[1024];
+	size_t track = 0;
+	size_t binStart = 0;
+	size_t binEnd = 0;
+
+	while(fgets(line, sizeof(line), srcCue) != NULL)
+	{
+		if (strncmp(line, "FILE", 4) == 0)
+		{
+			binStart = binEnd;
+			char *start = strchr(line, '"');
+			if (start == NULL) continue;
+			char *trackStart = strchr(start, '(');
+			if (trackStart == NULL) continue;
+
+			char *end = strchr(start + 1, '"');
+			if (end == NULL) continue;
+			char *trackEnd = strchr(trackStart, ')');
+			if (trackEnd == NULL) continue;
+
+			fputs("FILE ", dstCue);
+			fwrite(start, sizeof(char), trackStart - start - 1, dstCue);
+			fwrite(trackEnd + 1, sizeof(char), end - trackEnd - 1, dstCue);
+			fputs(end, dstCue);
+			
+			binEnd += binSizes[track];
+			track++;
+		}
+		else if(strncmp(line, "INDEX", 5) == 0)
+		{
+			int index, min, sec, frame;
+
+			if (sscanf(line, "INDEX %d %d:%d:%d", &index, &min, &sec, &frame) == 4)
+			{
+				size_t totalFrames = binStart / 2352;
+				totalFrames += (min * 60 + sec) * 75 + frame;
+
+				min = totalFrames / (60 * 75);
+				sec = (totalFrames / 75) % 60;
+				frame = totalFrames % 75;
+
+				fprintf(dstCue, "\t\tINDEX %02d %02d:%02d:%02d\n", index, min, sec, frame);
+			}
+		}
+		else
+		{
+			fputs(line, dstCue);
 		}
 	}
 }
