88a8dccb89329b41385cbde7c9ed1d626be761b3
William Correia <wburgard6@gmail.com>
2026-08-31 23:24:12 -0300

    feat: add freeBinList function

--- functions.h
+++ functions.h
@@ -11,6 +11,7 @@ char *setFullPath(char *dir, char *outputName);
 char **parseCue(FILE *cueFile, size_t *binCount);
 void copyFile(FILE *source, FILE *destination);
 void generateCue(FILE *srcCue, FILE *dstCue, size_t *binSizes);
+void freeBinList(char **binList, size_t binCount);
 size_t *getBinSizes(char *srcDir, char **binList, size_t binCount);
 
 #endif
