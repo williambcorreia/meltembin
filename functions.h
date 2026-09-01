75bff576f6bbc6db31076d671efa8d633de7baf2
William Correia <wburgard6@gmail.com>
2026-09-01 14:53:57 -0300

    chore: refactor params of copyfile function

--- functions.h
+++ functions.h
@@ -9,9 +9,9 @@ int checkFile(FILE *file);
 char *setOutputName(char *path, char *extension);
 char *setFullPath(char *dir, char *outputName);
 char **parseCue(FILE *cueFile, size_t *binCount);
-void copyFile(FILE *source, FILE *destination);
 void generateCue(FILE *srcCue, FILE *dstCue, size_t *binSizes, char *outputBinName);
 void freeBinList(char **binList, size_t binCount);
+void copyFile(FILE *source, FILE *destination, size_t totalSize, size_t *totalCopied);
 size_t *getBinSizes(char *srcDir, char **binList, size_t binCount);
 
 #endif
